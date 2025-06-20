33db105d40e25f871a4763279d44842ad1c2e737
carrot0217 <carrot0217@example.com>
2025-06-20 09:19:50 +0900

    supbase fix

--- supabase_client.py
+++ supabase_client.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()  # 로컬 실행 시 .env 사용 (Render에서는 무시됨)
 
-SUPABASE_URL = os.environ.get("SUPABASE_URL")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
-
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
